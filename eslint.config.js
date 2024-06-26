4d6ca381be374a0776a0565bb170812a6bdedfcd
sophiepavlova <olni.pavlova@gmail.com>
2024-06-26 14:34:55 -0700

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable prettier/prettier */
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  prettierConfig,
+  {
+    rules: {
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+      'import/extensions': ['error', { js: 'always' }],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+];
