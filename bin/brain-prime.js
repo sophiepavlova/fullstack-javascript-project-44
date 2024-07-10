63030c27ec1dd72cd2969c3b8a0fc836a4546e99
Olga Pavlova <olni.pavlova@gmail.com>
2024-07-10 14:47:15 -0700

    Create bin/brain-prime.js and src/games/prime.js.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+/* eslint-disable prettier/prettier */
+import startPrimeGame from '../src/games/prime.js';
+
+startPrimeGame();
---
+++ src/games/prime.js
@@ -0,0 +1,65 @@
+/* eslint-disable prettier/prettier */
+// import readlineSync from 'readline-sync';
+import {
+  getName,
+  giveTask,
+  getRandomNumber,
+  successMessage,
+  wrongGuessMessage,
+  incrementCount,
+  setCount,
+  getCount,
+  gameOver,
+  guess,
+} from '../index.js';
+
+function startPrimeGame() {
+  const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101];
+  let generatedNumber; 
+  let correctAnswer;
+  let guessIsTrue;
+
+    getName();
+    giveTask(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+
+    function getCorrectAnswer(){
+      if(primeNumbers.includes(generatedNumber)){
+        correctAnswer = true;
+      }else {
+        correctAnswer = false;
+      }
+    
+      return correctAnswer;
+    }
+    function isGuessCorrect() {
+      // even = rand % 2 === 0;
+      if (guessIsTrue === 'yes' && correctAnswer === false) {
+        wrongGuessMessage(guessIsTrue, `Correct answer was 'no'.`);
+        gameOver();
+      } else if (guessIsTrue === 'no' && correctAnswer === true) {
+        wrongGuessMessage(guessIsTrue, `Correct answer was 'yes'.`);
+        gameOver();
+      } else if (guessIsTrue !== 'yes' && guessIsTrue !== 'no') {
+        wrongGuessMessage(guessIsTrue, '');
+        gameOver();
+      } else {
+        incrementCount();
+        console.log('Correct!');
+        if (getCount() === 3) {
+          successMessage();
+        }
+      }
+    }
+
+    while (getCount() < 3) {
+      generatedNumber = getRandomNumber(1, 101);
+      guessIsTrue = guess(generatedNumber);
+      getCorrectAnswer();
+      isGuessCorrect(guessIsTrue)
+    }
+    setCount(0);
+  
+}
+export default startPrimeGame;
